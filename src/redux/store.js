a7e67c2f36611382adc006a5dc6c8afc0ffa8554
Pablo <pablo.ezequiel.fernandez@gmail.com>
2020-09-04 11:35:26 -0300

    Store y reducers

---
+++ src/redux/store.js
@@ -0,0 +1,15 @@
+import { createStore, combineReducers } from 'redux';
+import results from './reducers/results';
+import suggestions from './reducers/suggestions';
+import currentItem from './reducers/currentItem';
+
+
+const reducer = combineReducers({
+    results,
+    suggestions,
+    currentItem
+});
+
+const store = createStore(reducer);
+
+export default store;
